e03a6aba6723be16b1c7a552561889b9511790c4
XTruongN <tn647353@gmail.com>
2023-03-03 12:04:30 -0500

    add more files

---
+++ matrixRotate.js
@@ -0,0 +1,9 @@
+const matrixRotate = (M, a) => {
+  const nwArr = [];
+  for (let r = 0; r < M.length; r++) {
+    const row = [];
+    for (let c = 0; c < M[0].length; c++) {
+      row.push(0);
+    }
+  }
+};
